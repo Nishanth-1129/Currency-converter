ca3721804e1db68daacdce05ef99253e174b6ed6
Nishanth-1129 <mannenishanth2005@gmail.com>
2026-03-27 21:54:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,6 @@ function convert() {
     .then((val) => val.json())
     .then((val) => {
       // val.rates[to] gets the specific conversion result
-      // Change this line in your convert function:
       output_currency.value = Object.values(val.rates)[0].toFixed(5);
     })
     .catch((err) => {
